0246d1d293180006c48e4b664865872c061208aa
forever-chen <13935863746@163.com>
2018-02-25 17:38:35 +0800

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var port = 18080;
 var express = require('express');
 var app = express();
 var path = require('path');
+app.use(express.static(path.resolve(__dirname,'app')))
 app.use('/',function(req,res){
     res.sendFile(path.resolve(__dirname,'app/template/index.html'))
 })
